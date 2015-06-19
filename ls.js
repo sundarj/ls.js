87c1404dc32b43cebd12ce192d86c03df22c620e
radnus <sundarjoshi@hotmail.co.uk>
2015-06-19 14:02:47 +0000

    modular api

--- ls.js
+++ ls.js
@@ -1,30 +1,39 @@
 "use strict";
 
-var builtins = Object.create(null);
-builtins['+'] = function(x,y) { y = y || 0; return [(+x)+(+y)] };
+function LSJS(input) {
+    this.input = input || process.argv[2];
+    
+    this.builtins = Object.create(null);
+    this.builtins['+'] = function(x,y) { y = y || 0; return [(+x)+(+y)] };
+}
 
-var tokens = JSON.parse(process.argv[2].replace(/[()]/g, function(match) {
-    return match === '(' ? '[' : ']';
-}).replace(/\s/g, ',').replace(/[^,\[\]]/g, function(match) {
-    return '"'+match+'"';
-}));
-
-
-function parse(tokens) {
+LSJS.prototype.toArray = function() {
+    return JSON.parse(this.input.replace(/[()]/g, function(match) {
+        return match === '(' ? '[' : ']';
+    }).replace(/\s/g, ',').replace(/[^,\[\]]/g, function(match) {
+        return '"'+match+'"';
+    }));
+}
+    
+LSJS.prototype.parse = function(tokenlist) {
     var act, ctx = [];
-    tokens.forEach(function(token) {
-        if (token in builtins) {
-            act = builtins[token];
+    tokenlist.forEach(function(token) {
+        if (token in this.builtins) {
+            act = this.builtins[token];
         } else {
             if (token.map) {
-                ctx.push(parse(token));
+                ctx.push(this.parse(token));
             } else {
                 ctx.push(token);
             };
         } 
-    });
+    }, this);
     return act.apply(null, ctx)
 }
 
+LSJS.prototype.compiled = function() {
+    return this.parse(this.toArray(this.input));
+}
 
-console.log(parse(tokens));
+const lsjs = new LSJS;
+console.log(lsjs.compiled());
